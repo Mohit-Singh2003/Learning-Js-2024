67d3da4e9ce6319b1048719f5e2e7a3e594af170
Mohit Singh <91059735+Mohit-Singh2003@users.noreply.github.com>
2024-02-23 20:07:13 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+"use strict" ; //This keyword is used to treat all JS code as newer version
+console.log(3 + 3);
+let name = "Mohit"; //String Datatype
+let age = 21; // Int or Numeric Datatype
+let isLoggedIn = false // Boolean datatype 
+/* datatypes: number, bigint, string, boolean
+ Null
+Undefined
+*/
+console.log(typeof(age))
+console.log(typeof(undefined));
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("Mohit")
+const accountID = 144553
+let accountEmail = "mohit@gmail.com"
+// var accountPassword = "12345" 
+//  NEVER USE VAR 
+accountCity = "Noida"
+// Changing values
+// accountID = 123 can't modify const value
+accountEmail = "ms@ms.com"
+accountPassword = "121212"
+accountCity = "Delhi"
+console.table([accountEmail, accountID, accountPassword, accountCity])
