cf448481059a11c9db78d4a750bceca568f6a80d
Mohit Singh <91059735+Mohit-Singh2003@users.noreply.github.com>
2024-02-23 21:22:26 +0000

    conversion_datatypes

---
+++ 01_basics/conversion_operations.js
@@ -0,0 +1,12 @@
+let score = "Mohit";
+// console.log(typeof(score)); 
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+// if (isNaN(valueInNumber)) {
+//     console.log("Nahi horhi padhai");
+// }
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 and false -> 0
