a7b6f63a0049bdcb28585b4aef6cdfacc876bada
Mohit Singh <91059735+Mohit-Singh2003@users.noreply.github.com>
2024-02-26 14:02:22 +0000

    math and  num

--- 01_basics/conversion_operations.js
+++ 01_basics/conversion_operations.js
@@ -10,3 +10,4 @@ console.log(valueInNumber);
 // "33" -> 33
 // "33abc" -> NaN
 // true -> 1 and false -> 0
+// **************Operations**************
---
+++ 01_basics/strings.js
@@ -0,0 +1,22 @@
+// const name1 = "Mehul";
+// const age = 30;
+
+
+// const greeting = `Hello, my name is ${name1} and I am ${age} years old.`;
+
+// console.log(greeting);
+const str1 = "Hello";
+const str2 = " World";
+str1.concat(str2); // "Hello World"
+
+// console.log(`${str1.toUpperCase()}${str2}`); // "Hello World"
+// console.log(str1 + str2); // "Hello World"
+// const negValue = -1;
+// const posValue = Math.abs(negValue);
+// console.log(posValue);
+// let randomNumber = (Math.random());
+// console.log(randomNumber);
+let min = 1;
+let max = 10;
+let randomInteger = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(randomInteger);
